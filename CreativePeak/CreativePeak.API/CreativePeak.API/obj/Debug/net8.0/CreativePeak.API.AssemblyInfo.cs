e27eb919d2b048687b463eb4b34523f0d8478e5e
hadassa-w <wh.33681@gmail.com>
2025-03-24 11:56:24 +0200

    Update a Docker file.

--- CreativePeak/CreativePeak.API/CreativePeak.API/obj/Debug/net8.0/CreativePeak.API.AssemblyInfo.cs
+++ CreativePeak/CreativePeak.API/CreativePeak.API/obj/Debug/net8.0/CreativePeak.API.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CreativePeak.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cbc978b87df41e98405831dfa757d98ffd23745e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ab2f0d5881ea95d76cad62c1eac196d0d636f1f0")]
 [assembly: System.Reflection.AssemblyProductAttribute("CreativePeak.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CreativePeak.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
