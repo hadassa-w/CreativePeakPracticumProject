bd54cd646c80320d738f273eb62b388f898c0d22
hadassa-w <wh.33681@gmail.com>
2025-03-24 17:19:06 +0200

    Update projects Clients

--- CreativePeak/CreativePeakAdmin/src/app/app.component.spec.ts
+++ CreativePeak/CreativePeakAdmin/src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'CreativePeakViewers' title`, () => {
+  it(`should have the 'CreativePeakAdmin' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('CreativePeakViewers');
+    expect(app.title).toEqual('CreativePeakAdmin');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, CreativePeakViewers');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, CreativePeakAdmin');
   });
 });
