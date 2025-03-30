6050da24ce9aa3237fec4da74ee82b9f1288e9d4
hadassa-w <wh.33681@gmail.com>
2025-03-30 09:13:50 +0300

    Add a ConnectionStrings to API project

--- CreativePeak/CreativePeak.API/CreativePeak.API/Program.cs
+++ CreativePeak/CreativePeak.API/CreativePeak.API/Program.cs
@@ -135,16 +135,16 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 //if (app.Environment.IsDevelopment())
 //{
-app.UseSwaggerUI();
-app.UseSwagger();
+//app.UseSwaggerUI();
+//app.UseSwagger();
 //}
 
-//app.UseSwagger();
-//app.UseSwaggerUI(c =>
-//{
-//    c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
-//    c.RoutePrefix = string.Empty; // ���� ������ Swagger ������ ����
-//});
+app.UseSwagger();
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+    c.RoutePrefix = string.Empty; // ���� ������ Swagger ������ ����
+});
 
 app.UseCors("MyPolicy");
 
--- CreativePeak/CreativePeak.API/CreativePeak.API/obj/Debug/net8.0/CreativePeak.API.AssemblyInfo.cs
+++ CreativePeak/CreativePeak.API/CreativePeak.API/obj/Debug/net8.0/CreativePeak.API.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CreativePeak.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ccda335160efee66170b24df33dab21975e9b8db")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+96f6e4c351fcf4b5cbb4fb17aad4740b0748d587")]
 [assembly: System.Reflection.AssemblyProductAttribute("CreativePeak.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CreativePeak.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
