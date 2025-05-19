689e44b220e56448cd8a0e2b4433edac20712656
hadassa-w <wh.33681@gmail.com>
2025-05-19 22:19:03 +0300

    Updata User module

--- CreativePeak/CreativePeak.API/CreativePeak.Core/DTOs/UserDTO.cs
+++ CreativePeak/CreativePeak.API/CreativePeak.Core/DTOs/UserDTO.cs
@@ -13,5 +13,7 @@ namespace CreativePeak.Core.DTOs
         public string Email { get; set; }
         public string Phone { get; set; }
         public string Address { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
     }
 }
